4d371f76db43f9ba7d005044cdce3cc772713ae4
Praveen <prageck@gmail.com>
2014-10-31 23:52:22 +0530

    Basic route for /

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// app.use("/", express.static(__dirname + '/public'));
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+
+app.get('/', function(req, res){
+    res.sendfile('index.html');
+    // res.send('Hello World');
+});
 
 var server = app.listen(3000, function () {
 
