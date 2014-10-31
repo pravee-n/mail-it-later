61269ba36696f6ea5df296adacfc74f2756d9eb1
drecode feil <drecodeam@gmail.com>
2014-11-01 01:19:21 +0530

    added first code

--- public/main.js
+++ public/main.js
@@ -1,1 +1,34 @@
-console.log('Here I am');
+(function () {
+
+    var LandingPageController = (function() {
+        var settings = {
+            paletteAPI : 'http://www.colourlovers.com/api/palettes/top?jsonCallback=?'
+        };
+
+        var dom = {
+            'backgroundItem' : '.js-background-item'
+        };
+
+        function updatePalette() {
+            $.getJSON("http://www.colourlovers.com/api/palettes/random?jsonCallback=?",
+                function(allPalettes) {
+                    palettes = allPalettes;
+                    $( dom.backgroundItem ).each( function( index, element ) {
+                        $( this ).css( 'background', allPalettes[ index ] );
+                    });
+                    setTimeout( function(){
+                        updatePalette();
+                    }, 3000 );
+                }
+            );
+        }
+
+        (function init() {
+            updatePalette();
+        })()
+    })();
+
+
+
+
+})();
